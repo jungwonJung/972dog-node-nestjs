21f509d19a47538f6a8cd3f4f437ec6aae3cee78
JUNGganzi <wjdwjd1501@gmail.com>
2021-09-06 03:53:44 +0900

    nest 설정 진행중 swagger, env

---
+++ nest-backend/src/main.ts
@@ -0,0 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  
+
+  const config = new DocumentBuilder()
+  .setTitle('972Dog')
+  .setDescription('반려동물 사이즈 체크 서비스')
+  .setVersion('1.0')
+  .build();
+
+const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+app.enableCors({ origin: true, credentials: true });
+const PORT = process.env.PORT;
+await app.listen(PORT);
+}
+bootstrap();
